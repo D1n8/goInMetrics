4ff9187096c06733b098f89696865f09f9ec430c
D1n8 <mr.dinyar@mail.ru>
2026-09-12 14:26:42 +0500

    feat: add prettier and ProgressBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import ProgressBar from './ProgressBar';
+
 function App() {
   return (
-    <>
-    </>
-  )
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>
+        <ProgressBar label="Usage" progress={14} showValue />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
